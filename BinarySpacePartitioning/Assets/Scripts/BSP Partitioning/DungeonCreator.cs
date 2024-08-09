a85e6f1395b71b77878db5741434256c3ec55e64
wyllowyllo <105272901+wyllowyllo@users.noreply.github.com>
2024-08-09 15:39:00 +0900

    Generation of polygon type publicspace, and etc
    - logic of Generating the polygon _shaped space at the center of ground.
    - Inspector logic : variables management.

--- BinarySpacePartitioning/Assets/Scripts/BSP Partitioning/DungeonCreator.cs
+++ BinarySpacePartitioning/Assets/Scripts/BSP Partitioning/DungeonCreator.cs
@@ -1,23 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using Unity.VisualScripting;
 using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
+using Color = UnityEngine.Color;
 
 public enum PUBLICSPACE { none, left_bottom, right_bottom, left_top, right_top, center, plaza } //공용공간 생성 타입
 public class DungeonCreator : MonoBehaviour
 {
-    public PUBLICSPACE type;
-    public int dungeonWidth, dungeonHeight; //전체 그라운드 너비, 높이
-    public int roomWidthMin, roomHeightMin; //최소 방 크기
+    [Header("생성 타입")] public PUBLICSPACE type;
 
-    public int publicSpaceWidth, publicSpaceHeight; //공용공간 너비, 높이 (type이 none이면 적용되지 않음)
-    public int publicSpaceDoorNumOnWall; //공용공간의 벽면당 문 개수 (벽면당 최소 1개)
+    [Header("그라운드 크기")]
+    public int dungeonWidth;
+    public int dungeonHeight;
 
-    
+    [Header("최소 방 크기")]
+    [Tooltip("크기는 10보다 커야하며, dungeonWidth를 초과할 수 없습니다.")]
+    public int roomWidthMin;
+    [Tooltip("크기는 10보다 커야하며, dungeonHeight를 초과할 수 없습니다.")]
+    public int roomHeightMin;
+
+    [Header("공용공간 크기")]
+    [Tooltip("크기는 10보다 커야하며,dungeonWidth를 초과할 수 없습니다.")]
+    public int publicSpaceWidth; 
+    [Tooltip("크기는 10보다 커야하며,dungeonHeight를 초과할 수 없습니다.")]
+    public int publicSpaceHeight;
+
+    [Header("공용공간 벽면당 문 개수")]
+    public int publicSpaceDoorNumOnWall; 
+
+    [Header("Plaza 타입 반지름")]
+    [Tooltip("최대길이 = (그라운드 너비, 높이 중 작은 값의 절반)")]
+    public int plazaRadius; //plaza 타입 공용공간 반지름 길이
+    [Header("Plaza 타입 각 개수")]
+    public int polygon; //정n각형, 값은 최소 3
+
+    [Header("None 타입 최대분할 횟수")]
     public int maxIterations; //최대 분할 횟수 - 트리 높이 제한, 이 값이 작을수록 비교적 큰 방이 만들어짐 (PublicSpace 타입이 none일 경우에만 적용)
 
+    [Header("etc")]
+    [Tooltip("문의 크기는 벽의 크기보다 클 수 없습니다.")]
     public int entranceSize; //문 너비
    
    
@@ -32,16 +57,31 @@ public class DungeonCreator : MonoBehaviour
         if (type == PUBLICSPACE.none)
             return;
 
-        //최소 방 크기는 (전체 그라운드 크기 - 공용공간 크기) 보다 작아야 함
-        if(roomWidthMin > (dungeonWidth - publicSpaceWidth))
-        {           
-            publicSpaceWidth = dungeonWidth;
-            Debug.Log("방 최소너비가 (전체너비-공용공간 너비)보다 커서 publicSpaceWidth -> " + dungeonWidth + "으로 확장되었습니다");
+        if (type == PUBLICSPACE.plaza)
+        {
+            //최소 방 크기는 (전체 그라운드 크기 - 광장 크기) 보다 작아야 함
+            if (roomWidthMin > (dungeonWidth - plazaRadius*2))
+            {
+                if(dungeonWidth>dungeonHeight)
+                    plazaRadius = dungeonHeight / 2;
+                else
+                    plazaRadius = dungeonWidth / 2;
+                Debug.Log("방 최소너비 또는 최소높이가 (전체너비-광장 크기)보다 커서 plazaRadius -> " + plazaRadius + "으로 확장되었습니다");
+            }
         }
-        if (roomHeightMin > (dungeonHeight - publicSpaceHeight))
-        {          
-            publicSpaceHeight = dungeonHeight;
-            Debug.Log("방 최소높이 (전체높이-공용공간 높이)보다 커서 publicSpaceHeight -> " + dungeonHeight + "으로 확장되었습니다");
+        else
+        {
+            //최소 방 크기는 (전체 그라운드 크기 - 공용공간 크기) 보다 작아야 함
+            if (roomWidthMin > (dungeonWidth - publicSpaceWidth))
+            {
+                publicSpaceWidth = dungeonWidth;
+                Debug.Log("방 최소너비가 (전체너비-공용공간 너비)보다 커서 publicSpaceWidth -> " + dungeonWidth + "으로 확장되었습니다");
+            }
+            if (roomHeightMin > (dungeonHeight - publicSpaceHeight))
+            {
+                publicSpaceHeight = dungeonHeight;
+                Debug.Log("방 최소높이 (전체높이-공용공간 높이)보다 커서 publicSpaceHeight -> " + dungeonHeight + "으로 확장되었습니다");
+            }
         }
     }
    
@@ -95,11 +135,23 @@ public class DungeonCreator : MonoBehaviour
                 Visualize();
                 break;
             case PUBLICSPACE.plaza:
+                rootList = SplitTheSpace(type);
+                listOfRooms.Add(rootList[0]);
+
+                //공용공간(0번 인덱스) 제외하고 분할 시작
+                for (int i = 1; i < rootList.Count; i++)
+                {
+                    RoomNode rootNode = rootList[i];
+                    GenerateDungeon(rootNode.BottomLeftAreaCorner, rootNode.Width, rootNode.Height, type);
+                }
+
+                VisualizeForPlaza();
                 break;
         }
       
 
     }
+
     private void GenerateDungeon(Vector2Int startPoint, int totalWidth, int totalHeight, PUBLICSPACE type)
     {
         DungeonGenerator generator = new DungeonGenerator(totalWidth, totalHeight);
@@ -255,7 +307,7 @@ public class DungeonCreator : MonoBehaviour
                                 , 0);
             rootList.Add(node1);
 
-            //나머지 공간 분할
+            //나머지 공간 분할 (아래, 오른쪽, 위, 왼쪽 순서)
             if (leftBottomPoint.y > 0)
             {
                 RoomNode node2 = new RoomNode(new Vector2Int(leftBottomPoint.x, 0), new Vector2Int(rightTopPoint.x, leftBottomPoint.y)
@@ -288,7 +340,47 @@ public class DungeonCreator : MonoBehaviour
         }
         else if (type == PUBLICSPACE.plaza)
         {
+            Vector2Int center = new Vector2Int(dungeonWidth / 2, dungeonHeight / 2); //그라운드 정중앙
 
+            //plaza를 내접원으로 가지는 사각형이 있다고 가정하고 그 사각형 기준으로 분할
+            Vector2Int leftBottomPoint = new Vector2Int(center.x - plazaRadius, center.y - plazaRadius);
+            Vector2Int rightTopPoint = new Vector2Int(center.x + plazaRadius, center.y + plazaRadius);
+
+            //공용공간
+            RoomNode node1 = new RoomNode(leftBottomPoint, rightTopPoint
+                                , null
+                                , 0);
+            rootList.Add(node1);
+
+            //나머지 공간 분할 (아래, 오른쪽, 위, 왼쪽 순서)
+            if (leftBottomPoint.y > 0)
+            {
+                RoomNode node2 = new RoomNode(new Vector2Int(leftBottomPoint.x, 0), new Vector2Int(rightTopPoint.x, leftBottomPoint.y)
+                             , null
+                             , 0);
+                rootList.Add(node2);
+            }
+            if (dungeonWidth - rightTopPoint.x > 0)
+            {
+                RoomNode node3 = new RoomNode(new Vector2Int(rightTopPoint.x, 0), new Vector2Int(dungeonWidth, dungeonHeight)
+                             , null
+                             , 0);
+                rootList.Add(node3);
+            }
+            if (dungeonHeight - rightTopPoint.y > 0)
+            {
+                RoomNode node4 = new RoomNode(new Vector2Int(leftBottomPoint.x, rightTopPoint.y), new Vector2Int(rightTopPoint.x, dungeonHeight)
+                             , null
+                             , 0);
+                rootList.Add(node4);
+            }
+            if (leftBottomPoint.x > 0)
+            {
+                RoomNode node5 = new RoomNode(new Vector2Int(0, 0), new Vector2Int(leftBottomPoint.x, dungeonHeight)
+                             , null
+                             , 0);
+                rootList.Add(node5);
+            }
         }
 
 
@@ -299,6 +391,41 @@ public class DungeonCreator : MonoBehaviour
     void Visualize()
     {
         for (int i = 0; i < listOfRooms.Count; i++)
+        {
+            CreateMesh(listOfRooms[i].BottomLeftAreaCorner, listOfRooms[i].TopRightAreaCorner);
+        }
+    }
+    private void VisualizeForPlaza()
+    {
+        //정N각형 좌표(중점을 기준으로 좌표 원형배치)
+        Vector3[] vertices = new Vector3[polygon + 1];
+
+        vertices[0] = new Vector3(dungeonWidth/2, 0, dungeonHeight / 2) ;
+        for (int i = 1; i <= polygon; i++)
+        {
+            float angle = -i * (Mathf.PI * 2.0f) / polygon;
+            vertices[i] = new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle)) * plazaRadius + vertices[0];
+        }
+
+        //정N각형을 이루는 삼각형들 좌표
+        int[] triangles = new int[3 * polygon];
+        for (int i = 0; i < polygon - 1; i++)
+        {
+            triangles[i * 3] = 0;
+            triangles[i * 3 + 1] = i + 1;
+            triangles[i * 3 + 2] = i + 2;
+        }
+
+        triangles[3 * polygon - 3] = 0;
+        triangles[3 * polygon - 2] = polygon;
+        triangles[3 * polygon - 1] = 1;
+        
+        //정N각형 메쉬생성
+        CreateMeshForPlaza(vertices, triangles);
+
+
+        // 공용공간 외 나머지 방들
+        for (int i = 1; i < listOfRooms.Count; i++)
         {
             CreateMesh(listOfRooms[i].BottomLeftAreaCorner, listOfRooms[i].TopRightAreaCorner);
         }
@@ -361,6 +488,37 @@ public class DungeonCreator : MonoBehaviour
 
 
         line.enabled = true;
+
+    }
+    void CreateMeshForPlaza(Vector3[] vertices, int[] triangles)
+    {
+        Mesh mesh= new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        //mesh.RecalculateNormals();
+
+        GameObject plazaFloor = new GameObject("Plaza" , typeof(MeshFilter), typeof(MeshRenderer));
+
+        plazaFloor.transform.position = Vector3.zero;
+        plazaFloor.transform.localScale = Vector3.one;
+        plazaFloor.GetComponent<MeshFilter>().mesh = mesh;
+        plazaFloor.GetComponent<MeshRenderer>().material = material;
+
+        //OutLine
+        LineRenderer line = plazaFloor.AddComponent<LineRenderer>();
+        line.positionCount = polygon+1;
+        line.enabled = false;
+
+        for (int i = 1; i <= polygon; i++)
+        {
+            line.SetPosition(i-1, vertices[i]);
+    
+        }
+        line.SetPosition(polygon, vertices[1]);
+
+
+        line.enabled = true;
+
 
     }
 
@@ -684,6 +842,23 @@ public class DungeonCreator : MonoBehaviour
         }
 
         return onVertex;
+    }
+
+
+    
+    private void OnValidate() //인스펙터 상에서 변수 범위 제한
+    {
+        roomWidthMin = Mathf.Clamp(roomWidthMin, 10, dungeonWidth);
+        roomHeightMin = Mathf.Clamp(roomHeightMin, 10, dungeonHeight);
+
+        publicSpaceWidth= Mathf.Clamp(publicSpaceWidth, roomWidthMin, dungeonWidth);
+        publicSpaceHeight = Mathf.Clamp(publicSpaceHeight, roomHeightMin, dungeonWidth);
+        publicSpaceDoorNumOnWall = (publicSpaceDoorNumOnWall < 1) ? 1 : publicSpaceDoorNumOnWall;
+        
+        entranceSize = (entranceSize >= roomWidthMin) ? 5 : entranceSize;
+
+        plazaRadius = (dungeonWidth > dungeonHeight) ? Mathf.Clamp(plazaRadius, 5, dungeonHeight / 2) : Mathf.Clamp(plazaRadius, 5, dungeonWidth / 2);
+        polygon = (polygon < 3) ? 3 : polygon;
     }
 
 
---
+++ BinarySpacePartitioning/Assets/Scripts/BSP Partitioning/InspectorEditor.cs
@@ -0,0 +1,76 @@
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(DungeonCreator))]
+public class DungeonCreatorEditor : Editor
+{
+    SerializedProperty type;
+    SerializedProperty dungeonWidth;
+    SerializedProperty dungeonHeight;
+    SerializedProperty roomWidthMin;
+    SerializedProperty roomHeightMin;
+    SerializedProperty publicSpaceWidth;
+    SerializedProperty publicSpaceHeight;
+    SerializedProperty publicSpaceDoorNumOnWall;
+    SerializedProperty plazaRadius;
+    SerializedProperty polygon;
+    SerializedProperty maxIterations;
+    SerializedProperty entranceSize;
+    SerializedProperty material;
+
+    void OnEnable()
+    {
+        type = serializedObject.FindProperty("type");
+        dungeonWidth = serializedObject.FindProperty("dungeonWidth");
+        dungeonHeight = serializedObject.FindProperty("dungeonHeight");
+        roomWidthMin = serializedObject.FindProperty("roomWidthMin");
+        roomHeightMin = serializedObject.FindProperty("roomHeightMin");
+        publicSpaceWidth = serializedObject.FindProperty("publicSpaceWidth");
+        publicSpaceHeight = serializedObject.FindProperty("publicSpaceHeight");
+        publicSpaceDoorNumOnWall = serializedObject.FindProperty("publicSpaceDoorNumOnWall");
+        plazaRadius = serializedObject.FindProperty("plazaRadius");
+        polygon = serializedObject.FindProperty("polygon");
+        maxIterations = serializedObject.FindProperty("maxIterations");
+        entranceSize = serializedObject.FindProperty("entranceSize");
+        material = serializedObject.FindProperty("material");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUILayout.PropertyField(type);
+        EditorGUILayout.PropertyField(dungeonWidth);
+        EditorGUILayout.PropertyField(dungeonHeight);
+        EditorGUILayout.PropertyField(roomWidthMin);
+        EditorGUILayout.PropertyField(roomHeightMin);
+
+        // type이 none인 경우에만 비활성화
+        using (new EditorGUI.DisabledScope(type.enumValueIndex == (int)PUBLICSPACE.none))
+        {
+            EditorGUILayout.PropertyField(publicSpaceWidth);
+            EditorGUILayout.PropertyField(publicSpaceHeight);
+            EditorGUILayout.PropertyField(publicSpaceDoorNumOnWall);
+        }
+
+        // type이 plaza가 아닌 경우 비활성화
+        using (new EditorGUI.DisabledScope(type.enumValueIndex != (int)PUBLICSPACE.plaza))
+        {
+            EditorGUILayout.PropertyField(plazaRadius);
+            EditorGUILayout.PropertyField(polygon);
+        }
+
+        // type이 none이 아닌 경우 비활성화
+        using (new EditorGUI.DisabledScope(type.enumValueIndex != (int)PUBLICSPACE.none))
+        {
+            EditorGUILayout.PropertyField(maxIterations);
+        }
+       
+        EditorGUILayout.PropertyField(entranceSize);
+        EditorGUILayout.PropertyField(material);
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
+
+
